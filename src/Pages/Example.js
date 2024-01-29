f878142f1a67dec4c82d569548616367889acf8b
GracielaSu <grace.suluck@gmail.com>
2024-01-29 21:52:05 +0630

    Login Page

---
+++ src/Pages/Example.js
@@ -0,0 +1,23 @@
+import Button from '../Components/Button';
+
+function Login() {
+    return (
+      <>
+        <Button />
+        <div class="row">
+          <div class="col-6 bg-dark">sdf</div>
+          <div class="col-6 bg-primary">
+            <div class="row">
+            <div class="col-1 bg-success">
+                <div class="bg-info">sdf</div>
+            </div>
+            <div class="col-8 bg-danger">sdf</div>
+            <div class="col-2 bg-info">sdf</div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Login;
