220b26cb31809b8297489585bfdf33e0e6f6019d
Christofeee <chrizt0202@gmail.com>
2024-01-30 02:07:11 +0630

    login page design done

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,20 +4,20 @@ import Button from '../Components/Button';
 function Login() {
     return (
       <>
-        <div class="row" id="login">
-          <div class="col-6">
-            <div class="row h-100">
-              <div class="col-12"></div>
-              <div class="col-12" id="left-bottom-box">
-                <div id="login-box"><h3><pre><b>G e t   S t a r t e d</b></pre></h3></div>
+        <div class="container-flex" id="wrapper">
+          <div class="row h-100">
+            <div class="col-6" id="column1">
+              <div class="p-5 rounded text-white" id="login-box">
+                <div class="h1 pb-3">Get Started</div>
+                <div type="button" class="btn p-3 mt-5 d-block h3 rounded text-white">Login</div>
+                <div type="button" class="btn p-3 mt-5 d-block h3 rounded text-white">Register</div>
               </div>
             </div>
-            
-          </div>
-          <div class="col-6" id="right-box">
-            <div>
-              <pre>ohnt ohnt ohnt</pre>
-              <p>Find your stuffs here!</p>
+            <div class="col-6 text-white" id="column2">
+              <div>
+                <div class="h1">FIND IT FAST</div>
+                <div class="h6 mt-5">Find Your Electronic Stuffs Here!</div>
+              </div>
             </div>
           </div>
         </div>
